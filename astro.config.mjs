b203390ec9990207957020576d8c3167f31840f8
mfalaleev <mi.falaleev@yandex.ru>
2026-04-14 18:24:37 +0300

    - добавлены заметки

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import remarkGithubBlockquoteAlert from 'remark-github-blockquote-alert';
 
 export default defineConfig({
   site: 'https://www.computer-graphics.xyz',
   base: '/',
+  markdown: {
+    remarkPlugins: [remarkMath, remarkGithubBlockquoteAlert],
+    rehypePlugins: [rehypeKatex],
+  },
 });
